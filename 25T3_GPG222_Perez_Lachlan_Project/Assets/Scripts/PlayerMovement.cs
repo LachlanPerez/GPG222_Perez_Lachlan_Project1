b7817b6223bc6666c13ef2cc05fcd2bb938b121b
lockie0804 <1029065@student.sae.edu.au>
2025-10-20 11:16:53 +1100

    Added comments
    Added comments to what the code is trying to do

--- 25T3_GPG222_Perez_Lachlan_Project/Assets/Scripts/PlayerMovement.cs
+++ 25T3_GPG222_Perez_Lachlan_Project/Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,17 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float MoveSmoothTime;
-    [SerializeField] private float GravityStrength;
-    [SerializeField] private float JumpStrength;
-    [SerializeField] private float WalkSpeed;
-    [SerializeField] private float RunSpeed;
+    [SerializeField] private float MoveSmoothTime;// Makes the movement more smooth
+    [SerializeField] private float GravityStrength;// it will dictate the strength of gravity applied to the player
+    [SerializeField] private float JumpStrength;// the jump strength will dictate the velocity that wll be applied when the player jumps
+    [SerializeField] private float WalkSpeed;// the walk speed will give give modifiable values which will dictate the speed of the player will move depending on their sprinting state
+    [SerializeField] private float RunSpeed;// the run speed will give modifiable values at which will dictate which speeds the player will move depending on their sprint state
 
     private CharacterController Controller;
-    private Vector3 CurrentMoveVelocity;
-    private Vector3 MoveDampVelocity;
+    private Vector3 CurrentMoveVelocity;// this will be used to smoothen the player movement 
+    private Vector3 MoveDampVelocity;// this will be used to smoothen the player movement 
 
-    private Vector3 CurrentForceVelocity;
+    private Vector3 CurrentForceVelocity;// this will be the application of gravity onto the player of object 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,10 +31,10 @@ public class PlayerMovement : MonoBehaviour
         {
             x = Input.GetAxisRaw("Horizontal"),
             y = 0f,
-            z = Input.GetAxisRaw("Vertical")
+            z = Input.GetAxisRaw("Vertical")// raw function on the x and z-axis can get the left and right input keys and the forwards and back input keys with the vertical 
         };
 
-        if (PlayerInput.magnitude > 1f)
+        if (PlayerInput.magnitude > 1f)// if the magnitude is greater then one then it normlizes the vector
         {
             PlayerInput.Normalize();
         }
@@ -61,6 +61,4 @@ public class PlayerMovement : MonoBehaviour
         }
         Controller.Move(CurrentForceVelocity * Time.deltaTime);//uses the current move force velocity variable multiplied by delta time
     }
-
-    
 }
